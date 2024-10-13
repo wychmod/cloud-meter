015210aac388075fe4b53de2dfde55d87af1acaa
wychmod <545480453@qq.com>
2024-10-13 12:02:08 +0800

    feat: 📝 更新注释

--- cloud-engine/src/main/java/com/wychmod/controller/common/EnvironmentController.java
+++ cloud-engine/src/main/java/com/wychmod/controller/common/EnvironmentController.java
@@ -8,27 +8,55 @@ import com.wychmod.util.JsonFormatter;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * 环境控制器类，用于处理与环境相关的HTTP请求
+ */
 @RestController
 @RequestMapping("api/v1/environment")
 public class EnvironmentController {
+    /**
+     * 环境服务接口的实例，用于操作环境数据
+     */
     @Resource
     private EnvironmentService environmentService;
+
+    /**
+     * 获取项目环境列表
+     * @param projectId 项目ID，用于筛选特定项目的环境
+     * @return 返回包含环境列表的JsonFormatter对象
+     */
     @GetMapping("/list")
     public JsonFormatter list(@RequestParam("projectId") Long projectId){
         return JsonFormatter.buildSuccess(environmentService.list(projectId));
     }
+
+    /**
+     * 保存新的环境配置
+     * @param environmentSaveReq 包含新环境配置的请求对象
+     * @return 返回表示操作成功的JsonFormatter对象
+     */
     @PostMapping("/save")
     public JsonFormatter save(@RequestBody EnvironmentSaveReq environmentSaveReq){
         return JsonFormatter.buildSuccess(environmentService.save(environmentSaveReq));
     }
 
+    /**
+     * 更新现有环境配置
+     * @param environmentUpdateReq 包含更新环境配置的请求对象
+     * @return 返回表示操作成功的JsonFormatter对象
+     */
     @PostMapping("/update")
     public JsonFormatter save(@RequestBody EnvironmentUpdateReq environmentUpdateReq){
         return JsonFormatter.buildSuccess(environmentService.update(environmentUpdateReq));
     }
 
+    /**
+     * 删除指定的环境
+     * @param environmentDelReq 包含要删除的环境信息的请求对象
+     * @return 返回表示操作成功的JsonFormatter对象
+     */
     @PostMapping("/del ")
-    public JsonFormatter delete(@RequestBody EnvironmentDelReq environmentUpdateReq){
-        return JsonFormatter.buildSuccess(environmentService.delete(environmentUpdateReq.getId(),environmentUpdateReq.getProjectId()));
+    public JsonFormatter delete(@RequestBody EnvironmentDelReq environmentDelReq){
+        return JsonFormatter.buildSuccess(environmentService.delete(environmentDelReq.getId(),environmentDelReq.getProjectId()));
     }
 }
--- cloud-engine/src/main/java/com/wychmod/controller/stress/StressCaseController.java
+++ cloud-engine/src/main/java/com/wychmod/controller/stress/StressCaseController.java
@@ -9,30 +9,70 @@ import com.wychmod.util.JsonFormatter;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * 控制器类，用于处理与性能测试用例相关的HTTP请求
+ */
 @RestController
 @RequestMapping("api/v1/stress")
 public class StressCaseController {
+    /**
+     * 注入性能测试用例服务类
+     */
     @Resource
     private StressCaseService stressCaseService;
 
+    /**
+     * 根据项目ID和用例ID查询性能测试用例
+     *
+     * @param projectId 项目ID
+     * @param caseId 用例ID
+     * @return 返回查询到的用例信息
+     */
     @RequestMapping("find")
     public JsonFormatter findById(@RequestParam("projectId") Long projectId, @RequestParam("caseId") Long caseId){
         return JsonFormatter.buildSuccess(stressCaseService.findById(projectId,caseId));
     }
 
+    /**
+     * 删除性能测试用例
+     *
+     * @param stressCaseReq 包含要删除的用例信息的请求对象
+     * @return 返回删除成功的信息
+     */
     @PostMapping("/del")
     public JsonFormatter delete(@RequestBody StressCaseReq stressCaseReq){
         return JsonFormatter.buildSuccess(stressCaseService.delete(stressCaseReq.getId(),stressCaseReq.getProjectId()));
     }
+
+    /**
+     * 保存性能测试用例
+     *
+     * @param stressCaseSaveReq 包含要保存的用例信息的请求对象
+     * @return 返回保存成功的信息
+     */
     @PostMapping("/save")
     public JsonFormatter save(@RequestBody StressCaseSaveReq stressCaseSaveReq){
         return JsonFormatter.buildSuccess(stressCaseService.save(stressCaseSaveReq));
     }
 
+    /**
+     * 更新性能测试用例
+     *
+     * @param stressCaseUpdateReq 包含要更新的用例信息的请求对象
+     * @return 返回更新成功的信息
+     */
     @PostMapping("/update")
     public JsonFormatter update(@RequestBody StressCaseUpdateReq stressCaseUpdateReq){
         return JsonFormatter.buildSuccess(stressCaseService.update(stressCaseUpdateReq));
     }
+
+    /**
+     * 执行性能测试用例
+     *
+     * @param projectId 项目ID
+     * @param caseId 用例ID
+     * @return 返回执行成功的信息
+     */
     @GetMapping("/execute")
     public JsonFormatter execute(@RequestParam("projectId") Long projectId,@RequestParam("id") Long caseId){
         stressCaseService.execute(projectId,caseId);
