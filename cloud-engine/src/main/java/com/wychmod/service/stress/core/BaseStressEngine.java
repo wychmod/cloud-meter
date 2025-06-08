76a893ae8e1bf482f4a9c43ef7fcd954c281fdbd
wychmod <545480453@qq.com>
2025-06-09 01:01:23 +0800

    feat: ✨ 引擎采样器

---
+++ cloud-engine/src/main/java/com/wychmod/service/stress/core/BaseStressEngine.java
@@ -0,0 +1,14 @@
+package com.wychmod.service.stress.core;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @description: 压测引擎抽象类 4分钟
+ * @author: wychmod
+ * @date: 2024-11-03
+ */
+@Data
+@Slf4j
+public abstract class BaseStressEngine {
+}
